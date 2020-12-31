9f4f60ee806cca5619ca4508eb6de769e1e27932
TRT <ray@ryzen47.localdomain>
2020-12-31 13:06:44 -0500

    Restructuring the worker helpers

--- examples/runner/start.js
+++ examples/runner/start.js
@@ -5,11 +5,8 @@ const initializeThreads = ( data ) => {
 	let WebGL = new ArrayBuffer( 1 ) // temporary
 	if( !data ) data = {}
 
-	const agency = new Worker( data.agencyURL || 'agency.js' )
-	// should warn that the default is being used
-	agency.postMessage( { dynamics: a2d.port1 }, [ a2d.port1 ] )
-	agency.onmessage = console.log
-
+	const agency = AgencyHandler( a2d.port1, "agency.js" )
+	
 	const dynamics = new Worker( data.dynamicsURL || 'dynamics.js' )
 	dynamics.postMessage( { agency: a2d.port2, render: d2r.port1 }, [ a2d.port2, d2r.port1 ] )
 	dynamics.onmessage = console.log
